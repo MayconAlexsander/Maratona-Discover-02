93eb351196da88f10d44753f1b5eda3324a2e136
MayconAlexsander <mayconalexm@gmail.com>
2021-04-14 16:12:41 -0300

    Initial commit

---
+++ src/routes.js
@@ -0,0 +1,16 @@
+const express = require('express'); // Express: biblioteca para criar um servidor
+const routes = express.Router() // Router(): é uma parte do Express que vai criar os caminhos
+const ProfileController = require('./controllers/ProfileController')
+const JobController = require('./controllers/JobController')
+const DashboardController = require('./controllers/DashboardController')
+
+routes.get('/', DashboardController.index)
+routes.get('/job', JobController.create)
+routes.post('/job', JobController.save)
+routes.get('/job/:id', JobController.show)
+routes.post('/job/:id', JobController.update)
+routes.post('/job/delete/:id', JobController.delete)
+routes.get('/profile', ProfileController.index)
+routes.post('/profile', ProfileController.update)
+
+module.exports = routes;
